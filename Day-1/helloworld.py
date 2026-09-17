1de23efba8f7af9509b6ef9bfcc77a09a4971a9e
Anmon21 <bilguun313131@gmail.com>
2026-09-18 04:23:31 +0800

    First day

---
+++ Day-1/helloworld.py
@@ -0,0 +1,23 @@
+# Day 1 - 30DaysofPython Challenge
+print("Hello World!")   # print hello world
+
+print(2 + 7)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(1 + 2)   # addition(+)
+print(6 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print(8 / 2)   # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(5 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Bilguun'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Bilguun'}))  # Dictionary
+print(type({4.9, 3.14, 2.3}))    # Tuple
